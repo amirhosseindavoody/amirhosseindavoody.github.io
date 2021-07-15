c7b8c4019ebec06e758d0d7ccf3bd8b3e9ce73ed
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2021-07-15 12:48:56 -0700

    Creating good notifications

--- source.js
+++ source.js
@@ -26,7 +26,6 @@ let pomodoroDurationValue = 25;
 let permission = Notification.permission;
 
 function tryNotification() {
-    console.log("Trying a notification");
     if(permission === "granted") {
         showNotification();
      } else if(permission === "default"){
@@ -40,13 +39,10 @@ function showNotification() {
     // if(document.visibilityState === "visible) {
     //     return;
     // }
-
-    console.log("Creating a notification");
-
-    var title = "JavaScript Jeep";
-    icon = "image-url"
-    var body = "Message to be displayed";
-    var notification = new Notification('Title', { body, icon });
+    var title = "Pomodoro App";
+    icon = "tomato-icon.png"
+    var body = "Pomodoro finished";
+    var notification = new Notification(title, { body, icon });
     notification.onclick = () => { 
            notification.close();
            window.parent.focus();
@@ -196,19 +192,13 @@ function startPomodoro() {
         // If the count down is finished, write some text
         if (distance < 0) {
             timerDisplay.innerHTML = "Pomodoro Finished";
-            console.log("Trying to focus the window");
-            // window.focus();
-            window.alert("Pomodoro finished");
-            // tryNotification();
+            tryNotification();
             stopPomodoro();
         }
     }, 100);
 }
 
 function stopPomodoro() {
-    console.log("Stoping the pomodoro");
-    tryNotification();
-
     controlButton.innerHTML = "Start Pomodoro";
     controlButton.removeEventListener("click", stopPomodoro);
     controlButton.addEventListener("click", startPomodoro);
