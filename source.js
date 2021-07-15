70962b240f10024f3ec36ccc8d9adf01ccc795a2
Amirhossein Davoody <amirhossein.davoody@gmail.com>
2021-07-15 12:24:45 -0700

    Trying notifications

--- source.js
+++ source.js
@@ -23,6 +23,40 @@ const pomodoroDurationLabel = document.getElementById('pomodoro-duration-label')
 
 let pomodoroDurationValue = 25;
 
+let permission = Notification.permission;
+
+function tryNotification() {
+    if(permission === "granted") {
+        showNotification();
+     } else if(permission === "default"){
+        requestAndShowPermission();
+     } else {
+       alert("Use normal alert");
+     }
+}
+
+function showNotification() {
+    if(document.visibilityState === "visible) {
+        return;
+    }
+    var title = "JavaScript Jeep";
+    icon = "image-url"
+    var body = "Message to be displayed";
+    var notification = new Notification('Title', { body, icon });
+    notification.onclick = () => { 
+           notification.close();
+           window.parent.focus();
+    }
+ }
+
+ function requestAndShowPermission() {
+    Notification.requestPermission(function (permission) {
+       if (permission === "granted") {
+             showNotification();
+       }
+    });
+ }
+
 function create_time_table_plot() {
 
     plotArea.innerHTML = "";
@@ -161,12 +195,15 @@ function startPomodoro() {
             console.log("Trying to focus the window");
             // window.focus();
             window.alert("Pomodoro finished");
+            // tryNotification();
             stopPomodoro();
         }
     }, 100);
 }
 
 function stopPomodoro() {
+    tryNotification();
+
     controlButton.innerHTML = "Start Pomodoro";
     controlButton.removeEventListener("click", stopPomodoro);
     controlButton.addEventListener("click", startPomodoro);
